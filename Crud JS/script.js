b693a48d3031071af40fcc2742ad320b787251c2
Areeba-Ramzan1 <128275024+Areeba-Ramzan1@users.noreply.github.com>
2023-09-05 06:48:26 -0700

    Done

---
+++ Crud JS/script.js
@@ -0,0 +1,73 @@
+var rollV, nameV, addressV;
+
+function readForm() {
+    rollV = document.getElementById("rollNo").value;
+    nameV = document.getElementById("name").value;
+    addressV = document.getElementById("address").value;
+    console.log(rollV, nameV, addressV);
+}
+
+// Create
+document.getElementById("create").onclick = function () {
+    readForm();
+
+    firebase.database().ref("student/" + rollV)
+        .set(
+            {
+                rollNo: rollV,
+                name: nameV,
+                address: addressV,
+            }
+        );
+    alert("Data Created Successfully🙂");
+    document.getElementById("rollNo").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("address").value = "";
+}
+
+// Read
+document.getElementById("read").onclick = function(){
+    readForm();
+
+    firebase.database().ref("student/" + rollV)
+    .on("value" , function(snap)
+        {
+          document.getElementById("rollNo").value = snap.val().rollNo;
+          document.getElementById("name").value = snap.val().name;
+          document.getElementById("address").value = snap.val().address;
+        }
+    );
+    alert("Data read Successfully🙂");
+ }
+
+// Update 
+document.getElementById("update").onclick = function () {
+    readForm();
+
+    firebase.database().ref("student/" + rollV)
+        .update(
+            {
+                // rollNo: rollV,
+                name: nameV,
+                address: addressV,
+            }
+        );
+    alert("Data Updated Successfully🙂");
+    document.getElementById("rollNo").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("address").value = "";
+}
+
+// Delete
+document.getElementById("delete").onclick = function () {
+    readForm();
+
+    firebase.database().ref("student/" + rollV)
+        .remove();
+
+    alert("Data Deleted Successfully🙂");
+    document.getElementById("rollNo").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("address").value = "";
+}
+
